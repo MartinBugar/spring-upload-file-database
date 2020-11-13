85dc48449a85373f5d005b328aa3f9e2759b0252
Martin <martin1.bugar@gmail.com>
2020-11-13 15:55:12 +0100

    project created

---
+++ src/test/java/com/martyx/springuploadfiledatabase/SpringUploadFileDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martyx.springuploadfiledatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUploadFileDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
